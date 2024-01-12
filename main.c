b76c41f3703302057c9f5c9e51518e048988bc2d
admin <admin@htdocs.org>
2024-01-11 23:07:41 -0600

    awakening an old project

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
+/**		MAIN.C
+ * This is an x window client disguised as a de or wm.
+ */
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-
 #include <xcb/xcb.h>
 
 int
 main ()
 {
-    /* geometric objects */
+    /*
+    // geometric objects 
     xcb_point_t points[] = {
         {10, 10},
         {10, 20},
@@ -24,7 +27,7 @@ main ()
 
     xcb_point_t polyline[] = {
         {50, 10},
-        { 5, 20},     /* rest of points are relative */
+        {5 , 20},     // rest of points are relative 
         {25,-20},
         {10, 10}
     };
@@ -34,14 +37,14 @@ main ()
         {110, 25, 130, 60}
     };
 
-/*
-    xcb_rectangle_t rectangle1[] = {
-        { 1200-1000, 50, 1920-800, 20}
-    };
-*/
 
-    xcb_rectangle_t rectangle1[] = {
-        {0, 0, 1920, 1200}
+    xcb_rectangle_t rectangles[] = {
+        {1200-1000, 50, 1920-800, 20}
+    };
+
+    // define rectangles after you pull screen stats with xcb_setup_roots_iterator
+    xcb_rectangle_t rectangles[] = {
+        {0, 0, 1366, 768}
     };
 
     xcb_arc_t arcs[] = {
@@ -49,19 +52,31 @@ main ()
         {90, 100, 55, 40, 0, 270 << 6}
     };
 
+*/
+    xcb_point_t polyline[] = {
+        {50, 10},
+        { 5, 20},     // rest of points are relative 
+        {25,-20},
+        {10, 10}
+    };
 
     /* Open the connection to the X server */
     int connection_screen;
     xcb_connection_t *connection = xcb_connect (NULL, &connection_screen);
     if (xcb_connection_has_error(connection)) {
-        printf("xcb_connect returned an error on init");
+        printf("xcb_connect returned an error");
         exit(0);
     }
 
     /* Get the first screen */
-    xcb_screen_t *screen = xcb_setup_roots_iterator (xcb_get_setup (connection)).data;
+    xcb_screen_t *screen = xcb_setup_roots_iterator(xcb_get_setup(connection)).data;
 
-    /* Create black (foreground) graphic context */
+    // define rectangles after you pull screen stats with xcb_setup_roots_iterator
+    xcb_rectangle_t rectangles[] = {
+        {0, 0, (--screen->width_in_pixels), (--screen->height_in_pixels)}
+    };
+
+    /* Create [color] (foreground) graphic context */
     xcb_drawable_t  window     = screen->root;
     xcb_gcontext_t  foreground = xcb_generate_id (connection);
     uint32_t        mask       = XCB_GC_FOREGROUND | XCB_GC_GRAPHICS_EXPOSURES;
@@ -79,17 +94,18 @@ main ()
     values[0] = screen->black_pixel;
     values[1] = XCB_EVENT_MASK_EXPOSURE;
 
-    xcb_create_window (connection,                    /* connection          */
-                       XCB_COPY_FROM_PARENT,          /* depth               */
-                       window,                        /* window Id           */
-                       screen->root,                  /* parent window       */
-                       0, 
-                       0,                          /* x, y                */
-                       screen->width_in_pixels, screen->height_in_pixels,                      /* width, height       */
-                       0,                            /* border_width        */
-                       XCB_WINDOW_CLASS_INPUT_OUTPUT, /* class               */
-                       screen->root_visual,           /* visual              */
-                       mask, values );                /* masks */
+    xcb_create_window (connection,                    /* connection     */
+                       XCB_COPY_FROM_PARENT,          /* depth          */
+                       window,                        /* window Id      */
+                       screen->root,                  /* parent window  */
+                       0,                             /* x              */ 
+                       0,                             /* y              */
+                       screen->width_in_pixels,       /* width          */
+                       screen->height_in_pixels,      /* height         */
+                       0,                             /* border_width   */
+                       XCB_WINDOW_CLASS_INPUT_OUTPUT, /* class          */
+                       screen->root_visual,           /* visual         */
+                       mask, values );                /* masks          */
 
 
     /* Map the window on the screen and flush*/
@@ -123,10 +139,13 @@ main ()
                 xcb_poly_arc (connection, window, foreground, 2, arcs);
             */
                 // draw the rectangles 
-                xcb_poly_rectangle (connection, window, foreground, 1, rectangle1);
+                xcb_poly_rectangle (connection, window, foreground, 1, rectangles);
  
                 // fill rectangles
-                xcb_poly_fill_rectangle ( connection, window, foreground, 1, rectangle1);
+                //xcb_poly_fill_rectangle ( connection, window, foreground, 1, rectangles);
+
+                // We draw the polygonal line 
+                xcb_poly_line (connection, XCB_COORD_MODE_PREVIOUS, window, foreground, 4, polyline);
 
                 // flush the request 
                 xcb_flush (connection);
